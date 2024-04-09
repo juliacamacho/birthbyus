4cdfe7f470ae9c34c047f38464efbe2236378889
Julia Camacho <juliac123@gmail.com>
2024-04-08 21:48:41 -0400

    sort posts chronologically

--- src/components/Posts.vue
+++ src/components/Posts.vue
@@ -1,11 +1,15 @@
 <script setup>
   import Post from "./Post.vue"
   import posts from "../data/posts.json";
+
+  let sortedPosts = posts.sort(function(a, b) { 
+    return new Date(b.date) - new Date(a.date);
+  });
   
 </script>
 
 <template>
-  <div v-for="post in posts">
+  <div v-for="post in sortedPosts">
     <Post class="text-xl" :postData="post" />
   </div>
 </template>
